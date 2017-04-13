5ebb8311f5cdf16dfdf85af9c340865f8eee32ee
lisaztruong <lisaztruong@gmail.com>
2017-04-13 09:53:02 -0700

    finished

--- public/chat.js
+++ public/chat.js
@@ -1,26 +1,50 @@
-window.onload = function(){
+window.onload = function() {
 
-  var messages = [];
-  var socket = io.connect('http://localhost:3700');
-  var field = document.getElementById('field');
-  var sendButton = document.getElementById('send');
-  var content = document.getElementById('content');
+    var messages = [];
+    var socket = io.connect('http://localhost:3700');
+    var field = document.getElementById("field");
+    var sendButton = document.getElementById("send");
+    var content = document.getElementById("content");
+    var name = document.getElementById("name");
 
-  socket.on('message', function(data){
-    if (data.message) {
-      messages.push(data.message);
-      var html = '';
-      for (var i=0; i < messages.length; i++){
-        html += messages[i] + '<br />';
-      }
-      content.innerHTML = html;
-    } else {
-      console.log('There is a problem: ', data);
-    }
-  });
+    content.innerHTML = html;
+    content.scrollTop = content.scrollHeight;
 
-  sendButton.onclick = function(){
-    var text = field.value;
-    socket.emit('send', {message: text});
-  };
+    <script>
+    $("#content").scrollTop($("#content")[0].scrollHeight);
+    $(document).ready(function() {
+        $("#field").keyup(function(e) {
+            if(e.keyCode == 13) {
+                sendMessage();
+            }
+        });
+    });
+    </script>
+
+    socket.on('message', function (data) {
+        if(data.message) {
+            messages.push(data);
+            var html = '';
+            for(var i=0; i<messages.length; i++) {
+                html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
+                html += messages[i].message + '<br />';
+            }
+            content.innerHTML = html;
+        } else {
+            console.log("There is a problem:", data);
+        }
+    });
+
+    socket.emit('send', { message: text, username: name.value });
+    field.value = "";
+
+    sendButton.onclick = function() {
+        if(name.value == "") {
+            alert("Please type your name!");
+        } else {
+            var text = field.value;
+            socket.emit('send', { message: text, username: name.value });
+        }
+    };
+
 }
